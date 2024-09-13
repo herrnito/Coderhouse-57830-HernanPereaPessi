233c942e12ee1330dbf552c10588e13b3817fbd6
Hernan Perea Pessi <herno.rr@hotmail.com>
2024-09-13 18:31:26 -0300

    EnsayoPF

---
+++ proyecto/pasteleria/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Cliente)
+admin.site.register(models.Torta)
+admin.site.register(models.Pedido)
---
+++ proyecto/pasteleria/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+class Torta(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    torta = models.ForeignKey(Torta, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha_pedido = models.DateField()
+    estado = models.CharField(max_length=50, choices=[('pendiente', 'Pendiente'), ('completado', 'Completado')])
+
+    def __str__(self):
+        return f'Pedido de {self.cliente.nombre}'
