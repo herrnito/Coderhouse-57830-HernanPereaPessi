6316b58b6c2f0f76b753ca7864a841fda79db78b
Hernan Perea Pessi <herno.rr@hotmail.com>
2024-09-16 18:42:05 -0300

    EnsayoPF

--- proyecto/pasteleria/models.py
+++ proyecto/pasteleria/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
-    telefono = models.CharField(max_length=20)
-    email = models.EmailField()
+    telefono = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.nombre
 
 class Torta(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=6, decimal_places=2)
 
