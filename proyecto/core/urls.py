f6ca086bc7f5b5b134a9719b39dea4efec04cf6b
Hernan Perea Pessi <herno.rr@hotmail.com>
2024-09-20 15:52:31 -0300

    EnsayoPF

--- proyecto/core/urls.py
+++ proyecto/core/urls.py
@@ -1,11 +1,12 @@
 # from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
-
+    path('login', LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout', LogoutView.as_view(template_name='core/logout.html'), name='logout'),
 ]
